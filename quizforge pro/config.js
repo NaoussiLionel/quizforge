167c56aa0a71a4ffe37292b0a777cb0a469e8a90
NaoussiLionel <naoussilionel8@gmail.com>
2025-04-05 23:55:51 +0200

    Delete quizforge pro directory

--- quizforge pro/config.js
+++
@@ -1,7 +0,0 @@
-export const CONFIG = {
-  SUPABASE_URL: 'https://qnkockslvplcviyvuvft.supabase.co,'
-  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFua29ja3NsdnBsY3ZpeXZ1dmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MTc3MzMsImV4cCI6MjA1OTI5MzczM30.TNx8MW2b-xOsKKtblqQQrdwDmpY50OtxXxShh0blbvU',
-  API_ENDPOINT: 'https://api.deepinfra.com/v1/openai/chat/completions',
-  API_KEY: 'QCxoUtN1zmRL0QXniy9FK8fO4uccrDsy',
-  MAX_REQUESTS: 5
-};
